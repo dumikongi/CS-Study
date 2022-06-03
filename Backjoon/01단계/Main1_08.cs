40803e24a196d05e1da1a5016e7d985b2f1816ed
skqtpa@hanmail.net <skqtpa@hanmail.net>
2022-06-03 17:19:00 +0900

    코드테스트

---
+++ Backjoon/01단계/Main1_08.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon._01단계
+{
+    internal class Main1_08
+    {
+        static void main1(string[] args)
+        {
+            String[] num = Console.ReadLine().Split();
+
+            double n1 = double.Parse(num[0]);
+            double n2 = double.Parse(num[1]);
+
+            Console.WriteLine(n1 / n2);
+        }
+    }
+}
---
+++ Backjoon/01단계/Main1_10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon._01단계
+{
+    internal class Main1_10
+    {
+        static void Main1(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            
+            Console.Write(name);
+            Console.WriteLine("??!");
+        }
+    }
+}
+
+
