fa9d7e9452884c47ecaccc47b07dbce3d0157f9c
skqtpa@hanmail.net <skqtpa@hanmail.net>
2022-06-09 17:18:57 +0900

    과제용테스트

---
+++ Test/1/1_05.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1
+{
+    internal class _1_05
+    {
+        static void Main1(string[] args)
+        {
+            string strscore = "60,72,82,86,92";
+            string[] scores = strscore.Split(",");                     // { "60", "72", "82", "86", "92" }; 둘이 같은 것
+
+            int total = 0;
+       
+            for( int i = 0; i < scores.Length; i++)
+            {
+                total += int.Parse(scores[i]);
+            }
+
+            Console.WriteLine("총점 : " + total);
+
+        }
+    }
+}
---
+++ Test/1/1_06.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1
+{
+    internal class _1_06
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("점수 입력 : ");
+            int score = int.Parse(Console.ReadLine());
+            char grade;
+
+            Console.Write("입력한 점수는 {0}점이고, 등급은 ", score);
+
+            if (score >= 90 && score <= 100)
+            {
+                grade = 'A';
+            }
+            else if (score >= 80)
+            {
+                grade = 'B';
+            }
+            else if (score >= 70)
+            {
+                grade = 'C';
+            }
+            else if (score >= 60)
+            {
+                grade = 'D';
+            }
+            else if (score >= 50)
+            {
+                grade = 'E';
+            }
+            else
+            {
+                grade = 'F';
+            }
+
+            Console.WriteLine(grade + "입니다.");
+        }
+    }
+}
