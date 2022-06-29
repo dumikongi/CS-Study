9c8de9b4c22f9407b2215c9b31c182cda0fef40a
skqtpa@hanmail.net <skqtpa@hanmail.net>
2022-06-29 17:19:50 +0900

    수업예제

---
+++ Ch11/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("메시지박스1");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("제목과 내용이 있는 메시지박스", "메시지박스2");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("두개의 버튼을 갖는 메시지박스", "메시지박스3", MessageBoxButtons.YesNo);
+        }
+
+        private void btnUid_Click(object sender, EventArgs e)
+        {
+            string uid = txtUid.Text;
+            lbUid.Text = "결과 : " + uid;
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            lbName.Text = "결과 : " + txtName.Text;
+        }
+
+        private void btnHp_Click(object sender, EventArgs e)
+        {
+            lbHp.Text = "결과 : " + txtHp.Text;
+        }
+
+        private void btnFruit_Click(object sender, EventArgs e)
+        {
+            CheckBox[] chbFruits = { chbFruit1, chbFruit2, chbFruit3, chbFruit4, chbFruit5 };
+
+            List<string> fruits = new List<string>();
+
+            foreach (CheckBox chb in chbFruits)
+            {
+                if (chb.Checked)
+                {
+                    fruits.Add(chb.Text);
+                }
+            }
+
+            lbResultFruit.Text = "선택한 과일 : " + string.Join(", ", fruits);
+        }
+
+        private void btnGender_Click(object sender, EventArgs e)
+        {
+            if (rdMale.Checked)
+            {
+                lbResultGender.Text = "결과 : 남자";
+            }
+            else
+            {
+                lbResultGender.Text = "결과 : 여자";
+            }
+        }
+
+        private void btnYear_Click(object sender, EventArgs e)
+        {
+            if (rdYear1.Checked)
+            {
+                lbResultYear.Text = "결과 : 1학년";
+            }
+            else if (rdYear2.Checked)
+            {
+                lbResultYear.Text = "결과 : 2학년";
+            }
+            else if (rdYear3.Checked)
+            {
+                lbResultYear.Text = "결과 : 3학년";
+            }
+            else if (rdYear4.Checked)
+            {
+                lbResultYear.Text = "결과 : 4학년";
+            }
+        }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+    }
+}
